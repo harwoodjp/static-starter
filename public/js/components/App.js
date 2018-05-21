521cf0df4b7b65f710ff4db3562a6ddb9f9f591e
Justin Harwood <justin.harwood@benefitfocus.com>
2018-05-21 15:16:37 -0400

    react integration

---
+++ public/js/components/App.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const App = () => (
+  <a href="https://github.com/harwoodjp/static-starter">repo</a>
+)
+
+export default App
--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,12 @@
 import normalize from "normalize.css"
 import scss from "../css/style.scss"
+import React from "react"
+import { render } from "react-dom"
+
+import App from "./components/App"
+
+render(
+  <App />,
+  document.getElementById("app")
+)
+
